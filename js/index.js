e3a1207b75627fdf35c051f21ca5b4a196eac02a
Udsupu <usawijesekara@gmail.com>
2016-08-07 12:58:34 +0530

    Added confirm password

--- js/index.js
+++ js/index.js
@@ -17,4 +17,42 @@ myApp.controller("MainCtrl", function ($scope) {
        window.location = "./login.html";
  }
 });
+
+// myApp.directive("validPasswordC", function() {
+//   return {
+//     require: 'ngModel',
+//     scope: {
+
+//       reference: '=validPasswordC'
+
+//     },
+//     link: function(scope, elm, attrs, ctrl) {
+//       ctrl.$parsers.unshift(function(viewValue, $scope) {
+
+//         var noMatch = viewValue != scope.reference
+//         ctrl.$setValidity('noMatch', !noMatch);
+//         return (noMatch)?noMatch:!noMatch;
+//       });
+
+//       scope.$watch("reference", function(value) {;
+//         ctrl.$setValidity('noMatch', value === ctrl.$viewValue);
+
+//       });
+//     }
+//   }
+// });
+  myApp.directive('pwCheck', [function () {
+    return {
+      require: 'ngModel',
+      link: function (scope, elem, attrs, ctrl) {
+        var firstPassword = '#' + attrs.pwCheck;
+        elem.add(firstPassword).on('keyup', function () {
+          scope.$apply(function () {
+            var v = elem.val()===$(firstPassword).val();
+            ctrl.$setValidity('pwmatch', v);
+          });
+        });
+      }
+    }
+  }]);
  
